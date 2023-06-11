3692a245e65dbec9f0c12f56c47c7e244e9c78e0
VasilisDimitrakopoulos <127786101+VasilisDimitrakopoulos@users.noreply.github.com>
2023-06-11 22:26:39 +0300

    Add files via upload

---
+++ Code/Java Classes/AbsencesListVIew.java
@@ -0,0 +1,16 @@
+package com.example.school_hero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class AbsencesListVIew extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absnces_list_view);
+    }
+}
